152d376f86f6362f10a4b94a9ee150d04dd4b130
Kevin <kevin.ka_96@hotmail.com>
2021-07-12 14:11:15 +0200

    added script from cussbot

---
+++ public/js/app.js
@@ -0,0 +1,13 @@
+// var searchtext= document.getElementById("search-text").value;
+// console.log(searchtext);
+// console.log("hello");
+
+function searchfn(){
+    document.getElementById("dis1-text").innerHTML = "";
+    var searchtext= document.getElementById("search-text").value;
+    console.log(searchtext);
+    document.getElementById("dis1-text").innerHTML += " Hii there, u searched for "+ searchtext;
+    
+};
+
+
