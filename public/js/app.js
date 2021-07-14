8a39afdaa7765947224a204aefd318f5a6a8e984
Kevin <kevin.ka_96@hotmail.com>
2021-07-14 15:40:48 +0200

    it now displays the info as well

--- public/js/app.js
+++ public/js/app.js
@@ -2,17 +2,40 @@
 // console.log(searchtext);
 // console.log("hello");
 
+const resultObj = document.getElementById("dis1-text");
+
+
+let response = {}
+
 async function SearchToken() {
+    resultObj.innerText = "";
     let input = document.getElementById("search-text").value;
+
+    let results = {}
 
     var url = new URL('http://192.168.0.5:3000/search');
     var params = {id: input};
     url.search = new URLSearchParams(params);
 
-    await fetch(url).then(response =>  response.json())
-        .then(complete => console.log('Complete: ', complete))
+    const data = await fetch(url).then(response => response.json())
+        .then(complete => {
+            console.log('Complete: ', complete.description.en);
+            results = complete;
+            addObjects(complete);
+        })
         .catch((err) => console.log('Error app.js ', err));
+    
+    
+    
 }
+
+function addObjects(object) {
+    response = object;
+    let pTag = document.createElement('p');
+    pTag.innerHTML = object.description.en;
+    resultObj.appendChild(pTag);
+
+} 
 
 async function searchfn(){
     document.getElementById("dis1-text").innerHTML = "";
