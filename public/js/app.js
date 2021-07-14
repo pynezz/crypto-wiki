1cfc1152c5233cf1efb5325396f310e0f3c97c8f
Shikhar Singh <shikharsingh.original@gmail.com>
2021-07-14 23:13:27 +0530

    Added new view logic

--- public/js/app.js
+++ public/js/app.js
@@ -2,39 +2,32 @@
 // console.log(searchtext);
 // console.log("hello");
 
-const resultObj = document.getElementById("dis1-text");
-
-
 let response = {}
 
 //No need of async await if using callback
 function SearchToken() {
-    resultObj.innerText = "";
     let input = document.getElementById("search-text").value;
-
+    
     let results = {}
-
+    
     //Currently using localhost so it runs on everyone's machine
     //Change the url when deployed
     var url = new URL('http://localhost:3000/search');
     var params = {id: input};
     url.search = new URLSearchParams(params);
-
+    
     fetch(url).then(response => response.json())
-        .then(complete => {
-            console.log('Complete: ', complete.description.en);
-            results = complete;
-            addObjects(complete);
-        })
-        .catch((err) => console.log('Error app.js ', err));
+    .then(complete => {
+        console.log('Complete: ', complete.description.en);
+        results = complete;
+        addObjects(complete);
+    })
+    .catch((err) => console.log('Error app.js ', err));
 }
 
 function addObjects(object) {
-    response = object;
-    let pTag = document.createElement('p');
-    pTag.innerHTML = object.description.en;
-    resultObj.appendChild(pTag);
-
+    const resultObj = document.getElementById("dis1-text");
+    resultObj.innerHTML = object.description.en;
 } 
 
 //No use right now
